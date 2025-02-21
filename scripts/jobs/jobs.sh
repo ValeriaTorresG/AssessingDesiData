51182bf31ebcb73701b648f1a7a7f205ad326fa2
Valeria Torres <v.torresg23@uniandes.edu.co>
2025-02-21 13:09:05 -0500

    Update .sh path

--- scripts/jobs/jobs.sh
+++ scripts/jobs/jobs.sh
@@ -1,4 +1,4 @@
-python write_dataset.py \
+python ../write_dataset.py \
   --data_path "/Users/valeriatorresgomez/onedrive/tiny_dr1/spectro/redux/iron/tiles/cumulative" \
   --output_csv_flux "./data/flux_data.csv" \
   --output_csv_wave "./data/desi_wave_data.csv"
